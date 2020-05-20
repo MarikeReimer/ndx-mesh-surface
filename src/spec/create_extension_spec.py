d614834c79e10bc8468e8c5bfcf1b7c4c6030c1f
MarikeReimer <35542396+MarikeReimer@users.noreply.github.com>
2020-05-20 15:42:06 -0400

    First commit

--- src/spec/create_extension_spec.py
+++ src/spec/create_extension_spec.py
@@ -2,7 +2,7 @@
 
 import os.path
 
-from pynwb.spec import NWBNamespaceBuilder, export_spec, NWBGroupSpec, NWBAttributeSpec
+from pynwb.spec import NWBNamespaceBuilder, export_spec, NWBGroupSpec, NWBAttributeSpec, NWBDatasetSpec
 # TODO: import the following spec classes as needed
 # from pynwb.spec import NWBDatasetSpec, NWBLinkSpec, NWBDtypeSpec, NWBRefSpec
 
@@ -21,27 +21,32 @@ def main():
     # as in which namespace they are found
     # this is similar to specifying the Python modules that need to be imported
     # to use your new data types
-    ns_builder.include_type('ElectricalSeries', namespace='core')
+    ns_builder.include_type('NWBDataInterface', namespace='core')
+    ns_builder.include_type('PlaneSegmentation', namespace='core')
 
     # TODO: define your new data types
     # see https://pynwb.readthedocs.io/en/latest/extensions.html#extending-nwb
     # for more information
-    tetrode_series = NWBGroupSpec(
-        neurodata_type_def='TetrodeSeries',
-        neurodata_type_inc='ElectricalSeries',
-        doc=('An extension of ElectricalSeries to include the tetrode ID for '
-             'each time series.'),
-        attributes=[
-            NWBAttributeSpec(
-                name='trode_id',
-                doc='The tetrode ID.',
-                dtype='int32'
-            )
-        ],
-    )
 
+    #Mesh surface(Directly repurposed from CorticalSurfaces)
+    mesh_surface = NWBGroupSpec(doc='Vertices and faces which define a 3D mesh body',
+                        datasets=[
+                            NWBDatasetSpec(doc='faces for surface, indexes vertices', shape=(None, 3),
+                                            name='faces', dtype='uint', dims=('face_number', 'vertex_index')),
+                            NWBDatasetSpec(doc='vertices for surface, points in 3D space', shape=(None, 3),
+                                            name='vertices', dtype='float', dims=('vertex_number', 'xyz'))],
+                        neurodata_type_def='MeshSurface',
+                        neurodata_type_inc='NWBDataInterface')
+
+
+    #Extend PlaneSegmentation to add a group for mesh_surface
+    mesh_plane_segmentation = NWBGroupSpec('A PlaneSegmentation that stores mesh data',
+                                neurodata_type_inc='PlaneSegmentation',
+                                neurodata_type_def='MeshPlaneSegmentation',
+                                groups = [mesh_surface])
+                            
     # TODO: add all of your new data types to this list
-    new_data_types = [tetrode_series]
+    new_data_types = [mesh_plane_segmentation]
 
     # export the spec to yaml files in the spec folder
     output_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'spec'))
